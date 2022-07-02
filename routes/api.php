6c99253d510a48a06bcb2565fa06663e8ebd90e5
novalramdhani <novalfitrahramdhani@gmail.com>
2022-07-02 20:54:45 +0700

    add prefix v1

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', LoginController::class);
-Route::post('/register', RegisterController::class);
+Route::prefix('v1')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/register', RegisterController::class);
+});
