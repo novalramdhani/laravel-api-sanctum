c6708924b55f06453d1116f77f1c6ac2f1bdf26d
novalramdhani <novalfitrahramdhani@gmail.com>
2022-07-02 21:12:09 +0700

    modified http error for record

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -46,5 +47,18 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof NotFoundHttpException) {
+            return response()->json([
+                'code' => 404,
+                'status' => false,
+                'message' => 'Resource Not Found.'
+            ], 404);
+        }
+
+        return parent::render($request, $e);
     }
 }
